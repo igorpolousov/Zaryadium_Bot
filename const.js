a68572f4de4e25bc9f74bcbb442d42ec8d6c76ef
igorpolousov <igorpolousov@gmail.com>
2024-01-29 12:18:22 +0300

    + constants

---
+++ const.js
@@ -0,0 +1,5 @@
+const commands = `
+/start - Перезапустить бота
+/help - Помощь
+`
+module.exports.commands = commands
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const { Telegraf } = require('telegraf')
 const { message } = require('telegraf/filters')
+const text = require('./const')
 require('dotenv').config()
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on(message('sticker'), (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.start((ctx) => ctx.reply(`Привет ${ctx.message.from.first_name ? ctx.message.from.first_name : "Привет незнакомец"}`))
+bot.help((ctx) => ctx.reply(text.commands))
+
+//bot.on(message('sticker'), (ctx) => ctx.reply('👍'))
+//bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 bot.launch()
 
 // Enable graceful stop
